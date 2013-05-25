907f8f35bfcfda05a153c199d1670aa867ff79ff
Zbigniew Siciarz <antyqjon@gmail.com>
2013-05-25 17:31:23 +0200

    Added plot_data function.

--- python/ex1/ex1.py
+++ python/ex1/ex1.py
@@ -1,9 +1,21 @@
 import numpy as np
+from matplotlib import pyplot as plot
 
 
 def warmup_exercise():
     return np.identity(5)
 
 
+def plot_data(X, y):
+    plot.plot(X, y, 'rx', markersize=10)
+    plot.ylabel('Profit in $10,000s')
+    plot.xlabel('Population of City in 10,000s')
+    plot.show()
+
+
 if __name__ == '__main__':
-    print warmup_exercise()
+    data1 = np.loadtxt('../../octave/ex1/ex1data1.txt', delimiter=',')
+    X = data1[:,0]
+    y = data1[:,1]
+    m = y.size
+    plot_data(X, y)
