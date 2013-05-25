c4c2f3f3d69b0a0c0f374339227421a3886f41ad
Zbigniew Siciarz <antyqjon@gmail.com>
2013-05-25 13:54:47 +0200

    Whitespace fix in predict.m.

--- octave/mlclass-ex3/predict.m
+++ octave/mlclass-ex3/predict.m
@@ -7,12 +7,12 @@ function p = predict(Theta1, Theta2, X)
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 p = zeros(size(X, 1), 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
+%               your learned neural network. You should set p to a
 %               vector containing labels between 1 to num_labels.
 %
 % Hint: The max function might come in useful. In particular, the max
