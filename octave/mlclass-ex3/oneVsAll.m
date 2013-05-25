903fbdafc8cbe3f1ad2f2afe2b3d5bdf01234d85
Zbigniew Siciarz <antyqjon@gmail.com>
2013-05-25 13:28:40 +0200

    Implemented oneVsAll.m.

--- octave/mlclass-ex3/oneVsAll.m
+++ octave/mlclass-ex3/oneVsAll.m
@@ -50,13 +50,12 @@ X = [ones(m, 1) X];
 %
 
 
-
-
-
-
-
-
-
+for c = 1:num_labels
+     initial_theta = zeros(n + 1, 1);
+     options = optimset('GradObj', 'on', 'MaxIter', 50);
+     [theta] = fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), initial_theta, options);
+     all_theta(c, :) = theta';
+end;
 
 
 
