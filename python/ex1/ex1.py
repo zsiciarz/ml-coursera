e2e5d1d5eebcd413f1d8406ff954770aa7bfd51f
Zbigniew Siciarz <antyqjon@gmail.com>
2013-05-25 17:51:19 +0200

    Implemented compute_cost().

--- python/ex1/ex1.py
+++ python/ex1/ex1.py
@@ -13,9 +13,23 @@ def plot_data(X, y):
     plot.show()
 
 
+def compute_cost(X, y, theta):
+    m = y.size
+    cost = 0.0
+    for i in range(m):
+        cost += (np.dot(theta.transpose(), X[i]) - y[i]) ** 2
+    return cost / (2.0 * m)
+
+
 if __name__ == '__main__':
     data1 = np.loadtxt('../../octave/ex1/ex1data1.txt', delimiter=',')
-    X = data1[:,0]
-    y = data1[:,1]
+    # reshape to force X and y to be column vectors
+    X = data1[:,0].reshape(-1, 1)
+    y = data1[:,1].reshape(-1, 1)
     m = y.size
     plot_data(X, y)
+    X = np.concatenate((np.ones((m, 1)), X), axis=1)
+    theta = np.zeros((2, 1))
+    iterations = 1500
+    alpha = 0.01
+    print compute_cost(X, y, theta)
