667c0d2d2a8b0a72c46c879be72a7361616f398d
Zbigniew Siciarz <antyqjon@gmail.com>
2013-05-27 22:31:39 +0200

    Added price prediction to ex1_multi.m.

--- octave/ex1/ex1_multi.m
+++ octave/ex1/ex1_multi.m
@@ -104,8 +104,9 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
 
+x = [1 1650 3]';
+price = theta' * x;
 
 % ============================================================
 
@@ -149,8 +150,9 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
 
+x = [1 1650 3]';
+price = theta' * x;
 
 % ============================================================
 
