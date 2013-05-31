80cb671a752a4e5d601ccb2ec5debc1d62d8f903
Zbigniew Siciarz <antyqjon@gmail.com>
2013-05-31 13:08:34 +0200

    Implemented learning curve calculation.

--- octave/mlclass-ex5/learningCurve.m
+++ octave/mlclass-ex5/learningCurve.m
@@ -53,11 +53,13 @@ error_val   = zeros(m, 1);
 
 % ---------------------- Sample Solution ----------------------
 
-
-
-
-
-
+for i = 1:m
+    X_train = X(1:i, :);
+    y_train = y(1:i);
+    theta = trainLinearReg(X_train, y_train, lambda);
+    error_train(i) = linearRegCostFunction(X_train, y_train, theta, 0);
+    error_val(i) = linearRegCostFunction(Xval, yval, theta, 0);
+end;
 
 % -------------------------------------------------------------
 
