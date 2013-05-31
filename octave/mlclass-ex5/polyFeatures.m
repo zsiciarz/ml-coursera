696c3940cd15d0944890ad4190f6dd03532b67fa
Zbigniew Siciarz <antyqjon@gmail.com>
2013-05-31 13:10:37 +0200

    Whitespace fix in polyFeatures.m.

--- octave/mlclass-ex5/polyFeatures.m
+++ octave/mlclass-ex5/polyFeatures.m
@@ -10,10 +10,10 @@ function [X_poly] = polyFeatures(X, p)
 X_poly = zeros(numel(X), p);
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: Given a vector X, return a matrix X_poly where the p-th 
+% Instructions: Given a vector X, return a matrix X_poly where the p-th
 %               column of X contains the values of X to the p-th power.
 %
-% 
+%
 
 
 
