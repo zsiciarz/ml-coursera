42e8f3867bda636e76de0ee9375baac333abc15a
Zbigniew Siciarz <antyqjon@gmail.com>
2013-06-08 15:25:26 +0200

    Whitespace fix in ex6.m.

--- octave/mlclass-ex6/ex6.m
+++ octave/mlclass-ex6/ex6.m
@@ -3,7 +3,7 @@
 %
 %  Instructions
 %  ------------
-% 
+%
 %  This file contains code that helps you get started on the
 %  exercise. You will need to complete the following functions:
 %
@@ -20,14 +20,14 @@
 clear ; close all; clc
 
 %% =============== Part 1: Loading and Visualizing Data ================
-%  We start the exercise by first loading and visualizing the dataset. 
+%  We start the exercise by first loading and visualizing the dataset.
 %  The following code will load the dataset into your environment and plot
 %  the data.
 %
 
 fprintf('Loading and Visualizing Data ...\n')
 
-% Load from ex6data1: 
+% Load from ex6data1:
 % You will have X, y in your environment
 load('ex6data1.mat');
 
@@ -42,7 +42,7 @@ pause;
 %  decision boundary learned.
 %
 
-% Load from ex6data1: 
+% Load from ex6data1:
 % You will have X, y in your environment
 load('ex6data1.mat');
 
@@ -73,13 +73,13 @@ fprintf('Program paused. Press enter to continue.\n');
 pause;
 
 %% =============== Part 4: Visualizing Dataset 2 ================
-%  The following code will load the next dataset into your environment and 
-%  plot the data. 
+%  The following code will load the next dataset into your environment and
+%  plot the data.
 %
 
 fprintf('Loading and Visualizing Data ...\n')
 
-% Load from ex6data2: 
+% Load from ex6data2:
 % You will have X, y in your environment
 load('ex6data2.mat');
 
@@ -90,12 +90,12 @@ fprintf('Program paused. Press enter to continue.\n');
 pause;
 
 %% ========== Part 5: Training SVM with RBF Kernel (Dataset 2) ==========
-%  After you have implemented the kernel, we can now use it to train the 
+%  After you have implemented the kernel, we can now use it to train the
 %  SVM classifier.
-% 
+%
 fprintf('\nTraining SVM with RBF Kernel (this may take 1 to 2 minutes) ...\n');
 
-% Load from ex6data2: 
+% Load from ex6data2:
 % You will have X, y in your environment
 load('ex6data2.mat');
 
@@ -105,20 +105,20 @@ C = 1; sigma = 0.1;
 % We set the tolerance and max_passes lower here so that the code will run
 % faster. However, in practice, you will want to run the training to
 % convergence.
-model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma)); 
+model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
 visualizeBoundary(X, y, model);
 
 fprintf('Program paused. Press enter to continue.\n');
 pause;
 
 %% =============== Part 6: Visualizing Dataset 3 ================
-%  The following code will load the next dataset into your environment and 
-%  plot the data. 
+%  The following code will load the next dataset into your environment and
+%  plot the data.
 %
 
 fprintf('Loading and Visualizing Data ...\n')
 
-% Load from ex6data3: 
+% Load from ex6data3:
 % You will have X, y in your environment
 load('ex6data3.mat');
 
@@ -132,9 +132,9 @@ pause;
 
 %  This is a different dataset that you can use to experiment with. Try
 %  different values of C and sigma here.
-% 
+%
 
-% Load from ex6data3: 
+% Load from ex6data3:
 % You will have X, y in your environment
 load('ex6data3.mat');
 
