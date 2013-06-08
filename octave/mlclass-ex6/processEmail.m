5193edc56cc8a8a88885278e0c74e1a10783df36
Zbigniew Siciarz <antyqjon@gmail.com>
2013-06-08 16:33:07 +0200

    Creating word indices.

--- octave/mlclass-ex6/processEmail.m
+++ octave/mlclass-ex6/processEmail.m
@@ -97,14 +97,8 @@ while ~isempty(email_contents)
     %       str2). It will return 1 only if the two strings are equivalent.
     %
 
-
-
-
-
-
-
-
-
+    idx = find(strcmp(vocabList, str) == 1);
+    word_indices = [word_indices; idx];
 
     % =============================================================
 
