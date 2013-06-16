4dd18b2542257e000305250855158d0ac9bdf465
Zbigniew Siciarz <antyqjon@gmail.com>
2013-06-16 14:33:10 +0200

    Implemented data projection.

--- octave/mlclass-ex7/projectData.m
+++ octave/mlclass-ex7/projectData.m
@@ -18,8 +18,12 @@ Z = zeros(size(X, 1), K);
 %                    projection_k = x' * U(:, k);
 %
 
+Ureduce = U(:, 1:K);
 
-
+for i=1:size(X, 1)
+    z = Ureduce' * X(i, :)';
+    Z(i, :) = z;
+end;
 
 % =============================================================
 
