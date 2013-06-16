14b9cc5d79b4afbe868403603725fc757732a003
Zbigniew Siciarz <antyqjon@gmail.com>
2013-06-16 10:29:38 +0200

    Computing centroids.

--- octave/mlclass-ex7/computeCentroids.m
+++ octave/mlclass-ex7/computeCentroids.m
@@ -26,12 +26,12 @@ centroids = zeros(K, n);
 % Note: You can use a for-loop over the centroids to compute this.
 %
 
-
-
-
-
-
-
+for k=1:K
+    % use logical arrays for indexing
+    % see http://www.mathworks.com/help/matlab/math/matrix-indexing.html#bq7egb6-1
+    indexes = idx == k;
+    centroids(k, :) = mean(X(indexes, :));
+end;
 
 % =============================================================
 
