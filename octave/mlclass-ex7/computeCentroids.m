05e52dfa6c7dfc4de1346da436e4d00075067d0d
Zbigniew Siciarz <antyqjon@gmail.com>
2013-06-16 10:14:39 +0200

    Whitespace fix in computeCentroids.m.

--- octave/mlclass-ex7/computeCentroids.m
+++ octave/mlclass-ex7/computeCentroids.m
@@ -1,7 +1,7 @@
 function centroids = computeCentroids(X, idx, K)
-%COMPUTECENTROIDS returs the new centroids by computing the means of the 
+%COMPUTECENTROIDS returs the new centroids by computing the means of the
 %data points assigned to each centroid.
-%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by 
+%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by
 %   computing the means of the data points assigned to each centroid. It is
 %   given a dataset X where each row is a single data point, a vector
 %   idx of centroid assignments (i.e. each entry in range [1..K]) for each
