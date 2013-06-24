26ab10cc320cecb7b55bda800d8eb445017531c4
Zbigniew Siciarz <antyqjon@gmail.com>
2013-06-24 21:27:54 +0200

    Code style fixes.

--- python/ex2/ex2.py
+++ python/ex2/ex2.py
@@ -18,8 +18,7 @@ def sigmoid(x):
 
 if __name__ == '__main__':
     data1 = np.loadtxt('../../octave/mlclass-ex2/ex2data1.txt', delimiter=',')
-    X = data1[:,0:2]
+    X = data1[:, 0:2]
     # reshape to force y to be a column vector
-    y = data1[:,2].reshape(-1, 1)
-    m = y.size
+    y = data1[:, 2].reshape(-1, 1)
     plot_data(X, y)
