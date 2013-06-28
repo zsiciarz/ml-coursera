a5ce1fb1e469f348de0c18e978cf04c5e5d76850
Zbigniew Siciarz <antyqjon@gmail.com>
2013-06-28 22:06:49 +0200

    Regularized cost function and gradient.

--- python/ex2/ex2_reg.py
+++ python/ex2/ex2_reg.py
@@ -1,7 +1,7 @@
 import numpy as np
 from matplotlib import pyplot as plot
 
-from ex2 import plot_data
+from ex2 import plot_data, cost_function
 
 
 def map_feature(X1, X2, degree=6):
@@ -15,6 +15,18 @@ def map_feature(X1, X2, degree=6):
     return out
 
 
+def cost_function_reg(theta, X, y, lambda_):
+    """
+    Regularized logistic regression cost function.
+    """
+    m = X.shape[0]
+    cost, gradient = cost_function(theta, X, y)
+    reg_cost = (lambda_ / (2.0 * m)) * np.sum(theta[2:, :] ** 2)
+    reg_gradient = (lambda_ / m) * gradient
+    reg_gradient[0] = 0
+    return cost + reg_cost, gradient + reg_gradient
+
+
 if __name__ == '__main__':
     data1 = np.loadtxt('../../octave/mlclass-ex2/ex2data2.txt', delimiter=',')
     X = data1[:, 0:2]
@@ -22,3 +34,8 @@ if __name__ == '__main__':
     y = data1[:, 2].reshape(-1, 1)
     plot_data(X, y)
     X = map_feature(X[:, 0], X[:, 1])
+    m, n = X.shape
+    initial_theta = np.zeros((n, 1))
+    lambda_ = 1.0
+    cost, grad = cost_function_reg(initial_theta, X, y, lambda_)
+    print 'Cost at initial theta (zeros): %f' % cost
