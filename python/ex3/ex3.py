f24567790e8a2d2009058b6a8f93d85a57d272ad
Zbigniew Siciarz <antyqjon@gmail.com>
2013-07-02 23:30:23 +0200

    Added initial display_data function.

--- python/ex3/ex3.py
+++ python/ex3/ex3.py
@@ -1,10 +1,20 @@
 import numpy as np
 from matplotlib import pyplot as plot
+import matplotlib.cm as cm
 from scipy.io import loadmat
+
+
+def display_data(X):
+    """
+    TODO: reshape the data to look like 20x20 images
+    """
+    plot.imshow(X, cm.Greys)
+    plot.show()
 
 
 if __name__ == '__main__':
     data = loadmat('../../octave/mlclass-ex3/ex3data1.mat')
     X = data['X']
     y = data['y']
-    sel = np.random.permutation(X)
+    sel = np.random.permutation(X)[:100]
+    display_data(sel)
