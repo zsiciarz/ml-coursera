7664a5a55bddc330e6a4249fa2f59350cdab74e0
Zbigniew Siciarz <antyqjon@gmail.com>
2013-07-09 23:40:37 +0200

    Loading and displaying data from mat file in ex3_nn.

--- python/ex3/ex3_nn.py
+++ python/ex3/ex3_nn.py
@@ -2,8 +2,15 @@ import numpy as np
 from matplotlib import pyplot as plot
 from scipy.io import loadmat
 
+from ex3 import display_data
+
 
 if __name__ == '__main__':
+    data = loadmat('../../octave/mlclass-ex3/ex3data1.mat')
+    X = data['X']
+    y = data['y'].flatten()
+    sel = np.random.permutation(X)[:100]
+    display_data(sel)
     weights = loadmat('../../octave/mlclass-ex3/ex3weights.mat')
     Theta1 = weights['Theta1']
     Theta2 = weights['Theta2']
