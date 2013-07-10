c57e44af8b1f7c8f951573ec2411d63bcccdbd5e
Zbigniew Siciarz <antyqjon@gmail.com>
2013-07-10 18:08:20 +0200

    Added cost function prototype with unrolling of theta parameters.

--- python/ex4/ex4.py
+++ python/ex4/ex4.py
@@ -33,6 +33,12 @@ def sigmoid(x):
     return 1.0 / (1.0 + np.exp(-x))
 
 
+def nn_cost_function(nn_params, input_layer_size, hidden_layer_size, num_labels, X, y, lambda_):
+    boundary = input_layer_size * (hidden_layer_size - 1)
+    Theta1 = nn_params[:boundary].reshape((hidden_layer_size - 1, input_layer_size))
+    Theta2 = nn_params[boundary:].reshape((num_labels, hidden_layer_size)).shape
+
+
 if __name__ == '__main__':
     print 'Loading and Visualizing Data ...'
     data = loadmat('../../octave/mlclass-ex4/ex4data1.mat')
@@ -44,3 +50,15 @@ if __name__ == '__main__':
     weights = loadmat('../../octave/mlclass-ex4/ex4weights.mat')
     Theta1 = weights['Theta1']
     Theta2 = weights['Theta2']
+    m = X.shape[0]
+    X = np.concatenate((np.ones((m, 1)), X), axis=1)
+    nn_params = np.concatenate((Theta1.flatten(), Theta2.flatten()))
+    nn_cost_function(
+        nn_params,
+        input_layer_size=Theta1.shape[1],
+        hidden_layer_size=Theta2.shape[1],
+        num_labels=Theta2.shape[0],
+        X=X,
+        y=y,
+        lambda_=0
+    )
