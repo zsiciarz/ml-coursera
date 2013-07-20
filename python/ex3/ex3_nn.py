3bd7a3435d6dc2ddd82c7744ad3c7622b1e578fa
Zbigniew Siciarz <antyqjon@gmail.com>
2013-07-20 17:42:53 +0200

    Print function in ex3_nn.

--- python/ex3/ex3_nn.py
+++ python/ex3/ex3_nn.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import numpy as np
 from matplotlib import pyplot as plot
 from scipy.io import loadmat
@@ -15,24 +17,24 @@ def predict(Theta1, Theta2, X):
 
 
 if __name__ == '__main__':
-    print 'Loading and Visualizing Data ...'
+    print('Loading and Visualizing Data ...')
     data = loadmat('../../octave/mlclass-ex3/ex3data1.mat')
     X = data['X']
     y = data['y'].flatten()
     m = X.shape[0]
     sel = np.random.permutation(X)[:100]
     display_data(sel)
-    print 'Loading Saved Neural Network Parameters ...'
+    print('Loading Saved Neural Network Parameters ...')
     weights = loadmat('../../octave/mlclass-ex3/ex3weights.mat')
     Theta1 = weights['Theta1']
     Theta2 = weights['Theta2']
     X = np.concatenate((np.ones((m, 1)), X), axis=1)
     predictions = predict(Theta1, Theta2, X)
     accuracy = 100 * np.mean(predictions == y)
-    print 'Training set accuracy: %0.2f %%' % accuracy
+    print('Training set accuracy: %0.2f %%' % accuracy)
     random_X = np.random.permutation(X)
     for i in range(m):
         example = random_X[i].reshape(1, -1)
         prediction = predict(Theta1, Theta2, example)
-        print 'Prediction: %d (digit %d)' % (prediction, prediction % 10)
+        print('Prediction: %d (digit %d)' % (prediction, prediction % 10))
         display_data(example[:, 1:])
