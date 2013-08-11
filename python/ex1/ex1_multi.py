a9b6a47a56e4ba8896f5770762819734935f6a2c
Zbigniew Siciarz <antyqjon@gmail.com>
2013-08-11 13:17:46 +0200

    Added normalize_features function (like in ex5).

--- python/ex1/ex1_multi.py
+++ python/ex1/ex1_multi.py
@@ -3,6 +3,19 @@ from __future__ import print_function
 import numpy as np
 from matplotlib import pyplot as plot
 
+def normalize_features(X, mu=None, sigma=None):
+    m = X.shape[0]
+    if mu is None:
+        mu = np.mean(X, axis=0)
+    if sigma is None:
+        sigma = np.std(X, axis=0, ddof=1)
+    # don't change the intercept term
+    mu[0] = 0.0
+    sigma[0] = 1.0
+    for i in range(m):
+        X[i, :] = (X[i, :] - mu) / sigma
+    return X, mu, sigma
+
 
 if __name__ == '__main__':
     data = np.loadtxt('../../octave/ex1/ex1data2.txt', delimiter=',')
