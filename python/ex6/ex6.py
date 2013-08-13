10d543299245b9fc36aeb4418447bc0a7724668a
Zbigniew Siciarz <antyqjon@gmail.com>
2013-08-13 22:43:43 +0200

    Visualize input data.

--- python/ex6/ex6.py
+++ python/ex6/ex6.py
@@ -5,7 +5,17 @@ from matplotlib import pyplot as plot
 from scipy.io import loadmat
 
 
+def plot_data(X, y, show=True):
+    pos = y.nonzero()[0]
+    neg = (y == 0).nonzero()[0]
+    plot.plot(X[pos, 0], X[pos, 1], 'k+', markersize=7, linewidth=2)
+    plot.plot(X[neg, 0], X[neg, 1], 'ko', markerfacecolor='y', markersize=7, linewidth=2)
+    if show:
+        plot.show()
+
+
 if __name__ == '__main__':
     data = loadmat('../../octave/mlclass-ex6/ex6data1.mat')
     X = data['X']
     y = data['y'].flatten()
+    plot_data(X, y)
