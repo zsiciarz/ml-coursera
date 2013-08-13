2d375a0d437bf439bf1abd4578b31548ac314567
Zbigniew Siciarz <antyqjon@gmail.com>
2013-08-13 23:29:23 +0200

    Train SVM model.

--- python/ex6/ex6.py
+++ python/ex6/ex6.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 import numpy as np
 from matplotlib import pyplot as plot
 from scipy.io import loadmat
+import svmutil
 
 
 def plot_data(X, y, show=True):
@@ -19,3 +20,11 @@ if __name__ == '__main__':
     X = data['X']
     y = data['y'].flatten()
     plot_data(X, y)
+    print('Training Linear SVM ...')
+    C = 1
+    params = '-q -c %d -t %d -e %f' % (
+        C,  # cost
+        0,  # linear kernel
+        0.001,  # epsilon
+    )
+    model = svmutil.svm_train(y.tolist(), X.tolist(), params)
