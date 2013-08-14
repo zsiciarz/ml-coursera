5f481c5810515e8760f926d48e9f4b1a32307b60
Zbigniew Siciarz <antyqjon@gmail.com>
2013-08-14 17:52:31 +0200

    Added SVM decision boundary visualisation.

--- python/ex6/ex6.py
+++ python/ex6/ex6.py
@@ -15,11 +15,24 @@ def plot_data(X, y, show=True):
         plot.show()
 
 
+def visualize_boundary_linear(X, y, model):
+    support_vectors = model[1]
+    coeffs = model[3]
+    intercept = model[4]
+    normal_vector = coeffs.dot(support_vectors).flatten()
+    xp = np.linspace(np.amin(X[:, 0]), np.amax(X[:, 0]), 100)
+    yp = -(normal_vector[0] * xp + intercept) / normal_vector[1]
+    plot_data(X, y, show=False)
+    plot.plot(xp, yp, '-b')
+    plot.show()
+
+
 if __name__ == '__main__':
     data = loadmat('../../octave/mlclass-ex6/ex6data1.mat')
     X = np.require(data['X'], dtype=np.float64, requirements='C_CONTIGUOUS')
     y = np.require(data['y'].flatten(), dtype=np.float64)
     plot_data(X, y)
     print('Training Linear SVM ...')
-    C = 1
-    model = libsvm.fit(X, y, kernel='linear', tol=0.001, C=C)
+    C = 1.0
+    model = libsvm.fit(X, y, kernel='linear', tol=0.001, C=C, max_iter=20)
+    visualize_boundary_linear(X, y, model)
